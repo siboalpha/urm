e4cee33c744caaf553ae2da7f9b634704b774172
siboalpha <sibo.alphonsee@gmail.com>
2022-07-08 20:21:05 +0200

    added path to .evn

--- core/settings.py
+++ core/settings.py
@@ -2,7 +2,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+#load_dotenv()
+load_dotenv("/home/admin/urm/core/.env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
