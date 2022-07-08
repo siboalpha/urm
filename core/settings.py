f5b012bac5482b14525cf8964f4022bff70601b5
siboalpha <sibo.alphonsee@gmail.com>
2022-07-08 20:18:06 +0200

    added allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("MY_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['urm.rw', 'www.urm.rw']
 
 
 #Email Backend
